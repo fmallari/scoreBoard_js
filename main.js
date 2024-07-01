8061bb72f9d0196240ea2cbcda34e195ce01e36d
fmallari <fc.mallari@gmail.com>
2024-06-30 23:43:32 -0700

    increment buttons for home and guest scores

--- main.js
+++ main.js
@@ -1,33 +1,44 @@
+let homeScore = document.getElementById("home-score")
+let guestScore = document.getElementById("guest-score")
+
 let home = 12
 let guest = 5
 
-let homeScore = document.getElementById("home-score")
+//Increment home score according to assigned button   
 
-console.log(homeScore)
-
-
-
-
-//function will increment score count according to assigned button   
-
-function onePoint() {
-    home = home + 1 
-    console.log(home)
+function homeOne() {
+    home += 1 
     homeScore.textContent = home
 
  }
- onePoint()
 
-function twoPoints() {
-    home = home + 2 
+function homeTwo() {
+    home += 2 
     homeScore.textContent = home
  }
- twoPoints()
 
-function threePoints() {
-    home = home + 3 
+function homeThree() {
+    home += 3 
     homeScore.textContent = home
  }
- threePoints()
+
+ // increment guest score according to assigned button
+
+function guestOne() {
+    guest += 1 
+    guestScore.textContent = guest
+
+ }
+
+function guestTwo() {
+    guest += 2 
+    guestScore.textContent = guest
+ }
+
+function guestThree() {
+    guest += 3 
+    guestScore.textContent = guest
+ }
+
 
 
